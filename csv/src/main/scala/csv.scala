724d4faac51b98519e143728ce988611bcf47749
palutz <stefano.paluello@gmail.com>
2017-04-14 17:20:51 +0100

    workshop code

--- csv/src/main/scala/csv.scala
+++ csv/src/main/scala/csv.scala
@@ -1,3 +1,5 @@
+import shapeless._
+
 final case class Employee(
                            name    : String,
                            number  : Int,
@@ -23,21 +25,42 @@ object CsvEncoder {
         func(value)
     }
 
-   implicit val stringEnc: CsvEncoder[String] =
+   implicit def stringEnc: CsvEncoder[String] =
      pure(str => List(str))
 
-  implicit val intEnc: CsvEncoder[Int] =
+  implicit def intEnc: CsvEncoder[Int] =
      pure(num => List(num.toString))
 
-  implicit val booleanEnc: CsvEncoder[Boolean] =
+  implicit def booleanEnc: CsvEncoder[Boolean] =
     pure(bool => List(if(bool) "yes" else "no"))
 
-  implicit val iceCreamEnc: CsvEncoder[IceCream] =
-    pure(iceCream => List(
-      iceCream.name,
-      iceCream.numCherries.toString,
-      iceCream.inCone.toString
-    ))
+  // I have to comment this to use the genericEncoder, cause this is more speciic and Scala will use is if available
+//  implicit def iceCreamEnc: CsvEncoder[IceCream] =
+//    pure(iceCream => List(
+//      iceCream.name,
+//      iceCream.numCherries.toString,
+//      iceCream.inCone.toString
+//    ))
+
+  // adding implicit for shapeless...
+  implicit def hnilEnc: CsvEncoder[HNil] =
+    pure(hnil => Nil)
+
+  implicit def hconsEnc[H, T <: HList](implicit hEnc: CsvEncoder[H], tEnc: CsvEncoder[T]): CsvEncoder[H :: T] = {
+    pure {
+      case head :: tail =>
+        hEnc.encode(head) ++ tEnc.encode(tail)
+    }
+  }
+
+  // use dependant types to make a genercic encoder(s)
+  // GENERIC and Dependent types (CHECK IT !!!)
+  implicit def denericEnc[A, R] (
+                                    implicit
+                                    generic: Generic[A] { type Repr = R },  // or writing generic: Generic.Aux[A, R]
+                                    encoder: CsvEncoder[R]
+                                  ) : CsvEncoder[A] =
+      pure(a => encoder.encode(generic.to(a)))
 }
 
 
@@ -49,6 +72,10 @@ object Main extends Demo {
   println(encodeCsv("Dave"))
   println(encodeCsv(123))
   println(encodeCsv(true))
+  println(encodeCsv(IceCream("IceCream1", 0, false)))
+
+  val hlist = "Magnum" :: 0 :: false :: HNil
+  println(encodeCsv(hlist))
 }
 
 
